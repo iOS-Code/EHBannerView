2f6c98c5f5a02e3400d962a17bfc4d432ab4f5a6
岳琛 <yuechen@100tal.com>
2018-05-29 13:17:58 +0800

    no message

---
+++ README.md
@@ -0,0 +1,29 @@
+# YCBannerScrollView
+
+[![CI Status](https://img.shields.io/travis/yuechenwork@163.com/YCBannerScrollView.svg?style=flat)](https://travis-ci.org/yuechenwork@163.com/YCBannerScrollView)
+[![Version](https://img.shields.io/cocoapods/v/YCBannerScrollView.svg?style=flat)](https://cocoapods.org/pods/YCBannerScrollView)
+[![License](https://img.shields.io/cocoapods/l/YCBannerScrollView.svg?style=flat)](https://cocoapods.org/pods/YCBannerScrollView)
+[![Platform](https://img.shields.io/cocoapods/p/YCBannerScrollView.svg?style=flat)](https://cocoapods.org/pods/YCBannerScrollView)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+YCBannerScrollView is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'YCBannerScrollView'
+```
+
+## Author
+
+Conny Yue, yuechenwork@163.com
+
+## License
+
+YCBannerScrollView is available under the MIT license. See the LICENSE file for more info.
---
+++ YCBannerViewDemo/ViewController.m
@@ -0,0 +1,61 @@
+//
+//  ViewController.m
+//  YCBannerViewDemo
+//
+//  Created by 岳琛 on 2018/5/28.
+//  Copyright © 2018年 KMF-Engineering. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "YCBannerView.h"
+
+@interface ViewController ()<YCBannerViewDelegate>
+
+@property (strong, nonatomic) YCBannerView *bannerView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSArray *arr = @[ @"http://pic39.nipic.com/20140226/18071023_162553457000_2.jpg",//网络图片
+                      [UIImage imageNamed:@"1.jpg"],//本地图片，传image，不能传名称
+                      @"http://photo.l99.com/source/11/1330351552722_cxn26e.gif",//网络gif图片
+                      gifImageNamed(@"2.gif")//本地gif使用gifImageNamed(name)函数创建
+                     ];
+
+    NSArray * titleArr = @[@"网图", @"JPG", @"网络动态图", @"GIF"];
+    
+    _bannerView = [[YCBannerView alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 180)];
+    _bannerView.delegate = self;
+    _bannerView.placeholderImage = [UIImage imageNamed:@"placeholderImage.jpg"];
+    _bannerView.imageArray = arr;
+    _bannerView.describeArray = titleArr;
+    _bannerView.time = 3;
+    [_bannerView setPageImage:[UIImage imageNamed:@"other"] andCurrentPageImage:[UIImage imageNamed:@"current"]];
+    _bannerView.pagePosition = PositionBottomRight;
+    _bannerView.gifPlayMode = GifPlayModePauseWhenScroll;
+    [self.view addSubview:_bannerView];
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [YCBannerView clearDiskCache];
+}
+
+#pragma mark XRCarouselViewDelegate
+- (void)carouselView:(YCBannerView *)carouselView clickImageAtIndex:(NSInteger)index
+{
+    NSLog(@"点击了第%ld张图片", index);
+}
+
+@end
